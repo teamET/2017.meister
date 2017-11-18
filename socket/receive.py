49e0a0dab5d766ae6f1ea1dac957e06fb62529e7
sakakendo0321 <sakakendo0321@gmail.com>
2017-11-18 19:09:50 +0900

    json communication with receive.py send.py

--- socket/receive.py
+++ socket/receive.py
@@ -1,22 +1,15 @@
-import socket,flask
+import socket
 
 UDP_IP=""
 UDP_PORT=5005
 
-app=flask.Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "hello flask"
 
-def main():
-    app.run(host='0.0.0.0')
-
-    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    sock.bind((UDP_IP,UDP_PORT))
-    while True:
-            data,addr=sock.recvfrom(1024)
-            print("received",data)
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((UDP_IP,UDP_PORT))
+while True:
+    data,addr=sock.recvfrom(1024)
+    print("received",data)
 
 if __name__ == '__main__':
     main()
--- socket/send.py
+++ socket/send.py
@@ -1,10 +1,20 @@
 
-import socket
+import socket,json
 
 #raspberry pi ip address
-UDP_IP="192.168.137.93"
+UDP_IP="127.0.1.1"
 UDP_PORT=5005
-MESSAGE=b"Hello World"
 
-sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.sendto(MESSAGE,(UDP_IP,UDP_PORT))
+data={
+	'foo':1,
+	'aaa':2
+}
+
+def send():
+	print("send",MESSAGE)
+	sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	sock.sendto(MESSAGE.encode('utf-8'),(UDP_IP,UDP_PORT))
+
+MESSAGE=json.dumps(data)
+
+send()
