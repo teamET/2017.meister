09907710ea59635ab1b3f04196fbbb6db5fcd05a
sakakendo <sakakendo0321@gmail.com>
2018-02-26 08:13:48 +0000

    main.py conflict

--- pigpio/main.py
+++ pigpio/main.py
@@ -1,9 +1,17 @@
 import pigpio,time,socket,asyncore,requests,datetime,json,threading
 from contextlib import closing
 
+pins=(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27)
+class LED:
+    pi=pigpio.pi()
+    def __init__(self):
+        print('initialize led pin')
+        for i in pins:
+            self.pi.set_mode(i,pigpio.OUTPUT)
+    def up(self,pwm):
+        for pin in pins:
+            self.pi.set_PWM_dutycycle(pin,pwm[pin])
 
-#pulse width  modulation data
-#data={ 'right':0, 'left':0 }
 class Motor:
     last=[0,0,0,0]
     pi=pigpio.pi()
@@ -35,7 +43,7 @@ class Motor:
         if rate > 0:
             self.pi.set_PWM_dutycycle(pin1,rate)
         elif rate <0:
-            self.pi.ser_PWM_dutycycle(pin2.rate)
+            self.pi.set_PWM_dutycycle(pin2.rate)
         elif BREAK is True:
             self.pi.set_PWM_dutycycle(pin1,100)
             self.pi.set_PWM_dutycycle(pin2,100)
@@ -57,12 +65,12 @@ class  SubUdpServer(threading.Thread):
         return True
     def run(self):
         print('===  Sub Thread Starts===')
-#        print('sub id',id(self.data))
-        print('sub right',self.data["right"],'left',self.data["left"])
+        print("PORT",self.UDP_PORT)
         sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         with closing(sock):
             sock.bind((self.UDP_IP,self.UDP_PORT))
             while True:
+                cnt=0
                 mes=sock.recv(self.bufsize)
                 raw=mes.decode('utf-8')
                 print('mes',mes,'raw',raw)
@@ -70,31 +78,43 @@ class  SubUdpServer(threading.Thread):
                     print('Sub process is terminated')
                     break
                 elif raw is not '' : #and  self.is_json(raw) ==True:
-                    tmp=json.loads(raw)
-                    self.data['right']=tmp['right']
-                    self.data['left']=tmp['left']
+                    #led
+                    pwm_str=raw.split(',')
+                    for pwm in map(int,pwm_str):
+                        self.data[cnt]=pwm
+                        cnt+=1
+                    print(self.data)
                 else:
                     print('empty message or not json ')
-                print('Udp right',self.data["right"],'left',self.data["left"])
-                time.sleep(1)
+#                time.sleep(1)
 
 if __name__ == '__main__':
-    motor=Motor([14,15,23,24])
-    data={ 'right':0, 'left':0 }
+#    motor=Motor([14,15,23,24])
+#    data={ 'right':0, 'left':0 }
+#    print("max pin number:",pins[:-1:])
+    led_pwm=[0 for i in range(28)]
+#    for i in pins:
+#        led_pwm[i]=0
+    led=LED()
 
     #Udp Server setup
-    server=SubUdpServer(data)
+#    server=SubUdpServer(data)
+    server=SubUdpServer(led_pwm)
     server.setDaemon(True)
     server.start()
 #    time.sleep(1)
     print('=== Main Thread  Starts ===')
-#    print('main id ',id(data))
     
     while True:
-#        time.sleep(1)
-        print('motor.drive right',data["right"],'left',data["left"])
-        motor.drive(0,data['right'])
-        motor.drive(1,data['left'])
+        pass
+        '''
+        print('led_pwm',led_pwm)
+        led.up(led_pwm);
+        '''
+        """
+        for i in range(26):
+            led.up(i,led_pwm[i]);
+            """
 
 
 
