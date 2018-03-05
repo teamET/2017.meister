8be53097acf155c8e0c53317f3ce77579e014058
Spacekoki <koki.lego@gmail.com>
2018-03-05 15:28:22 +0900

    Add files via upload

---
+++ UI/gui.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import sys
+import tkinter
+
+
+root = tkinter.Tk()
+root.title(u"Software Title")
+root.geometry("400x300")
+
+#button
+led =Tkinter.Button(text=u'LED')
+Button.pack()
+
+root.mainloop()
