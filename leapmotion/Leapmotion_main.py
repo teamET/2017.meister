fc33b38aa0e9968b2d33ca324a164ee19ce423e7
sho0126hiro <sho0126hiro@yahoo.co.jp>
2018-03-08 11:44:23 +0900

    lagの補正

--- leapmotion/Leapmotion_main.py
+++ leapmotion/Leapmotion_main.py
@@ -142,11 +142,10 @@ def is_json(myjson):
         return False
     return True
 def send(message):
-
-
     print("message",message)
     sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     sock.sendto(message.encode('utf-8'),(UDP_IP,UDP_PORT))
+    sleep(0.01)
 
 import Leap
 class SampleListener(Leap.Listener):
